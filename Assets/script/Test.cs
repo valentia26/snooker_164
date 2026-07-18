d7824b76a9dad04bb5c84bd0617181af49dc1088
MooKae <naruechon05@gmail.com>
2026-07-18 09:57:48 +0700

    up

--- Assets/script/Test.cs
+++ Assets/script/Test.cs
@@ -1,11 +1,15 @@
 using UnityEngine;
 
-public class Farmer : MonoBehaviour
+public class Monobhaviour : Test
 {
+    void Awake()
+    {
+        Debug.Log("Awake");
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("start");
     }
 
     // Update is called once per frame
